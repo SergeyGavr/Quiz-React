dca3b6ea13fa0362caa25f8d5e30e0036fc4295d
SergeyGavr <cepera94@gmail.com>
2019-08-07 20:30:46 +0300

    Настройка приложения под redux

--- src/index.js
+++ src/index.js
@@ -3,12 +3,31 @@ import ReactDOM from 'react-dom';
 import {BrowserRouter as Router} from 'react-router-dom'
 import './index.css';
 import App from './App';
+import { createStore, compose, applyMiddleware } from 'redux'
+import { Provider } from 'react-redux'
+import rootRecucer from './store/reducers/rootReducer'
+import thunk from 'redux-thunk'
+
 import * as serviceWorker from './serviceWorker';
 
+const composeEnhancers =
+    typeof window === 'object' &&
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ?
+        window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
+        }) : compose;
+
+const store = createStore(
+    rootRecucer, composeEnhancers(
+        applyMiddleware(thunk)
+    ));
+
 const app = (
-  <Router>
-      <App />
-  </Router>
+    <Provider store={store}>
+        <Router>
+            <App />
+        </Router>
+    </Provider>
+
 );
 
 ReactDOM.render(app, document.getElementById('root'));
